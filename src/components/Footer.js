0165286c763711b7d681b2a6e4dc65ffbc09ad65
Peter G. Morales <pgmorales76@gmail.com>
2023-01-10 00:07:11 -0500

    Fix for error

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <>
+                <h2>
+                    The author is Peter G. Morales. All rights reversed.
+                </h2>
+            </>
+        )
+    }
+}
+
+export default Footer
