76610942550c9521b1ef1403fa3f2ca1e4e29e6a
Restyled.io <commits@restyled.io>
2023-01-10 05:30:35 +0000

    Restyled by prettier

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,25 @@ import HornedBeast from "./HornedBeast";
 
 class Main extends React.Component {
   render() {
-        return (
-            <>
-            
-                <HornedBeast title={"UniWhal"} imageUrl={"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"} description={
-      "A unicorn and a narwhal nuzzling their horns"} />
-                <HornedBeast title={"UniWhal"} imageUrl={"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"} description={"A unicorn and a narwhal nuzzling their horns"} />
-            </>
-        )
-    }
+    return (
+      <>
+        <HornedBeast
+          title={"UniWhal"}
+          imageUrl={
+            "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
+          }
+          description={"A unicorn and a narwhal nuzzling their horns"}
+        />
+        <HornedBeast
+          title={"UniWhal"}
+          imageUrl={
+            "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
+          }
+          description={"A unicorn and a narwhal nuzzling their horns"}
+        />
+      </>
+    );
+  }
 }
 
-export default Main
+export default Main;
