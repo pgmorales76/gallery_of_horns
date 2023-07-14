89efe77ffdb4bddcbe67faacae23a4eb6b3d9c32
Peter G. Morales <pgmorales76@gmail.com>
2023-07-14 00:44:12 -0400

    Passing props down to HornedBeast component

--- src/Home.jsx
+++ src/Home.jsx
@@ -37,7 +37,13 @@ class Home extends React.Component {
   render() {
     return (
       <main>
-        <HornedBeast/>
+        <HornedBeast
+          title={"UniWhal"}
+          description={"A unicorn and a narwhal nuzzling their horns"}
+          image_url={
+            "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
+          }
+        />
       </main>
     );
   }
