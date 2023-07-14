b17db04bbe7543810780babdb7235c46a9aee8b2
Peter G. Morales <pgmorales76@gmail.com>
2023-07-13 23:48:05 -0400

    Initial code for HornedBeast component

---
+++ src/HornedBeast.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class HornedBeast extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* TODO: Make this component dynamic */}
+        <h2>Replace with Title</h2>
+        {/* TODO: Add the image content */}
+        {/* <img/> */}
+        <p>Replace with description</p>
+      </div>
+    );
+  }
+}
+
+export default HornedBeast;
