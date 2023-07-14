d6d5fd8543b101dbafe60ca05fe45172cc057b91
Peter G. Morales <pgmorales76@gmail.com>
2023-07-14 01:14:16 -0400

    Added stretch goal to loop over the data to display the three HornedBeast components

--- src/Home.jsx
+++ src/Home.jsx
@@ -35,6 +35,18 @@ const beast_data = [
 
 class Home extends React.Component {
   render() {
+    const beast_component_array = [];
+
+    beast_data.forEach((beast) => {
+      beast_component_array.push(
+        <HornedBeast
+          title={beast.title}
+          description={beast.description}
+          image_url={beast.image_url}
+        />
+      );
+    });
+
     return (
       <main>
         {/* <HornedBeast
@@ -44,6 +56,7 @@ class Home extends React.Component {
             "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
           }
         /> */}
+        {beast_component_array}
       </main>
     );
   }
