2bd0a1b2634687738401233b344db36d71f6b299
Peter G. Morales <pgmorales76@gmail.com>
2023-07-23 18:46:39 -0400

    Added heart emoji and 'Favorites' with Card.Text

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Button, Card, Col } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 
 class HornedBeast extends React.Component {
   render() {
     return (
       <Col className="d-flex">
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={this.props.image_url} />
+          <Card.Img variant="top" src={this.props.image_url} height={200} />
           <Card.Body>
             <Card.Title>{this.props.title}</Card.Title>
             <Card.Text>{this.props.description}</Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Text>❤️ Favorites</Card.Text>
           </Card.Body>
         </Card>
       </Col>
