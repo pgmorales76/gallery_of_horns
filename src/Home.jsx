71a988a7797088bb13b82a6cf1fe94afbe675b6d
Peter G. Morales <pgmorales76@gmail.com>
2023-07-23 18:14:45 -0400

    Wrapped HornedBeast component in a Row component, and wrapped the Row component within a Container component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
 import beast_data from "./beast_data.json";
+import { Container, Row } from "react-bootstrap";
 
 class Home extends React.Component {
   render() {
-
     return (
-
       <main>
-        {beast_data.map((beast, idx) => (
-          <HornedBeast
-            title={beast.title}
-            description={beast.description}
-            image_url={beast.image_url}
-            key={idx}
-          />
-        ))}
+        <Container>
+          <Row>
+            {beast_data.map((beast, idx) => (
+              <HornedBeast
+                title={beast.title}
+                description={beast.description}
+                image_url={beast.image_url}
+                key={idx}
+              />
+            ))}
+          </Row>
+        </Container>
       </main>
-
     );
-
   }
-  
 }
 
 export default Home;
