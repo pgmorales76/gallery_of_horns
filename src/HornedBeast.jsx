448c777799193f288d5f092b96276b172a6460ca
Peter G. Morales <pgmorales76@gmail.com>
2023-07-23 13:08:31 -0400

    Imported Button and Card from react-bootstrap; and commented out div from lab_01

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
 
 class HornedBeast extends React.Component {
   render() {
     return (
-      <div>
-        {/* TODO: Make this component dynamic */}
-        <h2>{this.props.title}</h2>
-        {/* TODO: Add the image content */}
-        <img src={this.props.image_url} alt={this.props.title} width={250} />
-        <p>{this.props.description}</p>
-      </div>
+      // <div>
+      //   {/* TODO: Make this component dynamic */}
+      //   <h2>{this.props.title}</h2>
+      //   {/* TODO: Add the image content */}
+      //   <img src={this.props.image_url} alt={this.props.title} width={250} />
+      //   <p>{this.props.description}</p>
+      // </div>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     );
   }
 }
