3df00d420784c1886ac0bdbf6e239f44c2da7d6e
Peter G. Morales <pgmorales76@gmail.com>
2023-07-23 12:24:53 -0400

    Added beast_data.json and map() method to HornedBeast component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import beast_data from "./beast_data.json";
 
 class Home extends React.Component {
   render() {
-    const beast_component_array = [];
+    // const beast_component_array = [];
 
-    beast_data.forEach((beast) => {
-      beast_component_array.push(
-        <HornedBeast
-          title={beast.title}
-          description={beast.description}
-          image_url={beast.image_url}
-        />
-      );
-    });
+    // beast_data.forEach((beast) => {
+    //   beast_component_array.push(
+    //     <HornedBeast
+    //       title={beast.title}
+    //       description={beast.description}
+    //       image_url={beast.image_url}
+    //     />
+    //   );
+    // });
 
     return (
       <main>
-        {/* <HornedBeast
-          title={"UniWhal"}
-          description={"A unicorn and a narwhal nuzzling their horns"}
-          image_url={
-            "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-          }
-        /> */}
-        {beast_component_array}
+        {beast_data.map((beast, idx) => (
+          <HornedBeast
+            title={beast.title}
+            description={beast.description}
+            image_url={beast.image_url}
+            key={idx}
+          />
+        ))}
       </main>
     );
   }
