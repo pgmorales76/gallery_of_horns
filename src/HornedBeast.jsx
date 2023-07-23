4cde85786c6a9051866485a7c5c3806279f63119
Peter G. Morales <pgmorales76@gmail.com>
2023-07-23 13:21:21 -0400

    Added props from Home component, and removed div from lab_01

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -4,24 +4,14 @@ import { Button, Card } from "react-bootstrap";
 class HornedBeast extends React.Component {
   render() {
     return (
-      // <div>
-      //   {/* TODO: Make this component dynamic */}
-      //   <h2>{this.props.title}</h2>
-      //   {/* TODO: Add the image content */}
-      //   <img src={this.props.image_url} alt={this.props.title} width={250} />
-      //   <p>{this.props.description}</p>
-      // </div>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={this.props.image_url} />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
