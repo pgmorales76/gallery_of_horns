59e1169b49cfb6512391a1356696bef95dedc425
Peter G. Morales <pgmorales76@gmail.com>
2023-07-23 18:31:14 -0400

    Wrapped outer Card component with a Col component, and added flex styling attribute

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Col } from "react-bootstrap";
 
 class HornedBeast extends React.Component {
   render() {
     return (
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={this.props.image_url} />
-        <Card.Body>
-          <Card.Title>{this.props.title}</Card.Title>
-          <Card.Text>{this.props.description}</Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <Col className="d-flex">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={this.props.image_url} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>{this.props.description}</Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Col>
     );
   }
 }
