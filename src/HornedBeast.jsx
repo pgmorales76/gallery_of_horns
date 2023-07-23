6330eb825da145df8a0876bbf8795b6f7cae7696
Peter G. Morales <pgmorales76@gmail.com>
2023-07-23 19:03:32 -0400

    Added state to keep track of favorites

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import { Card, Col } from "react-bootstrap";
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0,
+    };
+  }
+
+  increment_favorites= () => {
+    this.setState({favorites: this.favorites + 1})
+  }
+
   render() {
     return (
       <Col className="d-flex">
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={this.props.image_url} height={200} />
+          <Card.Img onClick={this.increment_favorites} variant="top" src={this.props.image_url} height={200} />
           <Card.Body>
             <Card.Title>{this.props.title}</Card.Title>
             <Card.Text>{this.props.description}</Card.Text>
-            <Card.Text>❤️ Favorites</Card.Text>
+            <Card.Text>{this.state.favorites} ❤️</Card.Text>
           </Card.Body>
         </Card>
       </Col>
