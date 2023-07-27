0e7bc2977a1c263047b9c779fbb9436aa286994c
Peter G. Morales <pgmorales76@gmail.com>
2023-07-26 22:31:10 -0400

    Passed map method as prop for Home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,22 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-import beast_data from "./beast_data.json";
-import { Container, Row } from "react-bootstrap";
 
 class Home extends React.Component {
   render() {
     return (
-      <main>
-        <Container>
-          <Row>
-            {beast_data.map((beast, idx) => (
-              <HornedBeast
-                title={beast.title}
-                description={beast.description}
-                image_url={beast.image_url}
-                key={idx}
-              />
-            ))}
-          </Row>
-        </Container>
-      </main>
-    );
+      <>
+        {
+          this.props.beast_data.map((beast, idx) =>
+          <HornedBeast 
+            key={idx}
+            title={beast.title}
+            image_url={beast.image_url}
+            description={beast.description}
+          />
+          )
+        }
+      </>
+    )
   }
 }
 
