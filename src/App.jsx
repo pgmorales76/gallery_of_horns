ac5a78895545d6d89e8296e7a03ef2bdbb11f7ca
Peter G. Morales <pgmorales76@gmail.com>
2023-07-26 22:11:51 -0400

    Formatting

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
     return (
       <>
         <Header />
-        <Home beast_data={beast_data}/>
+        <Home beast_data={beast_data} />
         <Footer />
       </>
     );
