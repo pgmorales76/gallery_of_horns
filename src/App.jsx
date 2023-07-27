1088b6c9df939da79e8763937143a09fd04c9798
Peter G. Morales <pgmorales76@gmail.com>
2023-07-26 23:48:12 -0400

    Created state, and passed in a function, as a prop, to the Home component

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,23 @@ import Home from "./Home";
 import SelectedBeast from "./SelectedBeast";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beast: {},
+    };
+  }
+
+  set_selected_beast = (beast) => this.setState({ beast: beast });
+
   render() {
     return (
       <>
         <Header />
-        <Home beast_data={beast_data} />
+        <Home
+          beast_data={beast_data}
+          set_selected_beast={this.set_selected_beast}
+        />
         <Footer />
         <SelectedBeast />
       </>
