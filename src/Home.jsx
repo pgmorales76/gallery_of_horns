0ff46cdf0822b42f520a79e76f50ee5d54762110
Peter G. Morales <pgmorales76@gmail.com>
2023-07-26 22:48:19 -0400

    Nested props within a Row component; and, nested the Row component within a Container component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import { Container, Row } from "react-bootstrap";
 
 class Home extends React.Component {
   render() {
     return (
-      <>
-        {this.props.beast_data.map((beast, idx) => (
-          <HornedBeast
-            key={idx}
-            title={beast.title}
-            image_url={beast.image_url}
-            description={beast.description}
-          />
-        ))}
-      </>
+      <Container>
+        <Row>
+          {this.props.beast_data.map((beast, idx) => (
+            <HornedBeast
+              key={idx}
+              title={beast.title}
+              image_url={beast.image_url}
+              description={beast.description}
+            />
+          ))}
+        </Row>
+      </Container>
     );
   }
 }
