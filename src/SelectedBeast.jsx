10207965500338293122f0a2b1175eda23159bbf
Peter G. Morales <pgmorales76@gmail.com>
2023-07-27 00:33:49 -0400

    Commented out Modal

--- src/SelectedBeast.jsx
+++ src/SelectedBeast.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Modal } from "react-bootstrap";
+// import { Modal } from "react-bootstrap";
 
 class SelectedBeast extends React.Component {
   render() {
     return (
-      <Modal show={} onHide={}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer></Modal.Footer>
-      </Modal>
+        <></>
+    //   <Modal show={} onHide={}>
+    //     <Modal.Header closeButton>
+    //       <Modal.Title>Modal heading</Modal.Title>
+    //     </Modal.Header>
+    //     <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+    //     <Modal.Footer></Modal.Footer>
+    //   </Modal>
     );
   }
 }
