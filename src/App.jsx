73e73263c29b36a1dc1605d6918da1b6adae353f
Peter G. Morales <pgmorales76@gmail.com>
2023-07-31 13:26:42 -0400

    Added if, else statement for filtered data

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,14 @@ class App extends React.Component {
 
   update_filtered_data = (e) => {
     const targeted_beast = e.target.value;
-    const updated_data = this.state.filtered_beast_data.filter(beast => beast.horns === targeted_beast);
-    this.setState({filtered_beast_data: updated_data});
+    if (targeted_beast === "All") {
+      this.setState({ filtered_beast_data: beast_data });
+    } else {
+      const updated_data = beast_data.filter(
+        (beast) => beast.horns === targeted_beast
+      );
+      this.setState({ filtered_beast_data: updated_data });
+    }
   };
 
   render() {
@@ -36,14 +42,10 @@ class App extends React.Component {
 
         <Form.Select onChange={this.update_filtered_data}>
           <option value={"All"}>All Beasts</option>
-          <option value={"Zero"}>
-            Zero Horns (Why would there be a Horned Beast with zero Horns?
-            CMON!)
-          </option>
-          <option value={"One"}>One Horn</option>
-          <option value={"Two"}>Two Horns</option>
-          <option value={"Three"}>Three Horns</option>
-          <option value={"One Hundred"}>One Hundred Horns</option>
+          <option value={"1"}>One Horn</option>
+          <option value={"2"}>Two Horns</option>
+          <option value={"3"}>Three Horns</option>
+          <option value={"100"}>One Hundred Horns</option>
         </Form.Select>
         <Home
           beast_data={this.state.filtered_beast_data}
